143daa513ae047b92cd90a1ed6725414778a6dcb
Sergej Shakhov <97630517+sergej239@users.noreply.github.com>
2022-01-16 12:19:32 +0200

    Update 1_3.js

--- 1_3.js
+++ 1_3.js
@@ -1,7 +1,7 @@
 let val;
 if (val=='a') {
     console.log('a');
-} else if (val=='b' || val=='c' || val=='d' || val=='e') {
+} else if (val==='b' || val==='c' || val==='d' || val==='e') {
     console.log('others');
 } else {
     console.log('unknown');
