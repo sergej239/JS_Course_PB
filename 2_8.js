bd52f76c75a347bcb414610900a0a2adf63d68b7
Sergej Shakhov <97630517+sergej239@users.noreply.github.com>
2022-01-21 01:31:05 +0200

    Update 2_8.js

--- 2_8.js
+++ 2_8.js
@@ -1,1 +1,15 @@
-s
+function f(arr) {
+    if (Array.isArray(arr)) {
+        if (arr.length == 0) {
+            throw new Error('parameter can not be an empty')
+        } else {
+            console.log(arr.splice(0,1)) 
+            if (arr.length !=0) {
+                    f(arr);
+                }
+        }
+    } else {
+        throw new Error('parameter type should be an array')
+    }
+}
+f([1,2,3]);
