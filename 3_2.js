8794438dc1b2cd7592ef9f116190e30ca7267e42
Sergej Shakhov <97630517+sergej239@users.noreply.github.com>
2022-01-28 22:06:22 +0200

    Update 3_2.js

--- 3_2.js
+++ 3_2.js
@@ -1,17 +1,22 @@
-function forEach(arr, callback) {
+function filter(arr, callback) {
+    let rez=[];
     if (arguments.length==2 && Array.isArray(arr) && typeof callback === 'function') {
         for(let i=0; i<arr.length; i++) {
-            if (i in arr) {
-                callback(arr[i], i, arr);
+            if (i in arr) { // Проверка на не пустой элемент массива
+                if (callback(arr[i], i, arr)) {
+                    rez.push(arr[i])
+                }
             }
         }
     } else {
         throw new Error('Error in arguments');
     }
+return rez;
 }
 
 const arr = [1,2,3,4,5];
 
-forEach(arr, function (item, i, arr) { //вызов функции
-     console.log(item, i, arr);
+let f=filter(arr, function (item,i,arr) { //вызов функции
+     return item>3;
 })
+console.log(f);
