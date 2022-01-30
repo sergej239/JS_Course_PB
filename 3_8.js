d01ef06a101993e7166189b7f8974259e7aafdd0
Sergej Shakhov <97630517+sergej239@users.noreply.github.com>
2022-01-30 20:31:22 +0200

    Update 3_8.js

--- 3_8.js
+++ 3_8.js
@@ -1,1 +1,23 @@
+function f(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('parameter type should be an Array')
+    }
+    let sum = 0;
+    while (arr.length !==0) {
+        var cur = arr.shift();
+        if (typeof cur !== 'number' && !Array.isArray(cur)) {
+            throw new Error('Was found neither a number nor an array');
+        }
+        if (Array.isArray(cur)) {
+            arr.unshift(...cur);
+            continue;
+        }
 
+    sum += cur;
+    }
+    return sum;
+}
+
+const arr=[[[1,2],[1,2]],[[2,1],[1,2]]];
+let result=f(arr);
+console.log(result);
